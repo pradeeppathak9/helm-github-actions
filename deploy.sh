1ff7293b58da3b2e9f81d655e6cb83e9ef94c0e5
Ubuntu <ubuntu@ip-172-31-41-248.ap-south-1.compute.internal>
2023-01-24 19:08:12 +0000

    testing actions

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+export KUBECONFIG='${{ secrets.KUBECONFIG }}'
+helm init --kube-context CONTEXT_NAME --upgrade
+
 for filename in ./all-values/*.yaml; do
     echo $filename
     helm install --generate-name ./charts/random-app-chart/ -f $filename 
